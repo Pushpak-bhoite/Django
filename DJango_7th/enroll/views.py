d5d4ef9367f2a7488788d23db4e460497a464e4d
Pushpak bhoite <bhoitepushpak6@gmail.com>
2022-11-04 23:45:37 +0530

    This Changes for creating new model Class & ckeck data is feched or not in html

--- DJango_7th/enroll/views.py
+++ DJango_7th/enroll/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from enroll.models import Student_1
+
 # Create your views here.
+def stuDetails(request):
+    stud = Student_1.objects.all()
+    return render (request,'enroll/stuDetails.html',{'stu':stud})
